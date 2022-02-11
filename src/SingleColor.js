05389f2469faadc58ef903cc862d4b3c178b0460
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-11 21:06:13 +0200

    add function to copy color

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -5,12 +5,20 @@ const SingleColor = ({ rgb, weight, index }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
   const hex = rgbToHex(...rgb);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
   return (
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
       onClick={() => {
         setAlert(true);
+        navigator.clipboard.writeText(hex);
       }}
     >
       <p className="percent-value">{weight}%</p>
