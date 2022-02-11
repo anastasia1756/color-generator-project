15a7093d2920b53d58977753a9ccbdc2f497e6a3
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-11 21:10:09 +0200

    fix bug

---
+++ build/precache-manifest.46244aefca74794396f162103b86aba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd9bb58fad5cbe15273477895acfea1",
+    "url": "/color-generator--project/index.html"
+  },
+  {
+    "revision": "8528af7f126bc5eda947",
+    "url": "/color-generator--project/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "82d364fb58ab7f735059",
+    "url": "/color-generator--project/static/js/2.72847889.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/color-generator--project/static/js/2.72847889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8528af7f126bc5eda947",
+    "url": "/color-generator--project/static/js/main.5cccac78.chunk.js"
+  },
+  {
+    "revision": "471ab0717307063a30b8",
+    "url": "/color-generator--project/static/js/runtime-main.21e1e53f.js"
+  }
+]);
